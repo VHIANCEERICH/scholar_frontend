fc21e4289fe7f86b63656c6337f1d603e9df1e5b
ErichGuden <erich.guden@jmc.edu.ph>
2026-05-01 09:38:10 +0800

    ako nani

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,35 +14,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-  const MyApp({Key? key}) : super(key: key);
-
-
+  const MyApp({super.key});
 
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       home: Scaffold(
-
         appBar: AppBar(
-
           title: const Text('Scholar Flutter'),
-
         ),
-
         body: const Center(
-
           child: Text('Welcome to Scholar Flutter!'),
-
         ),
-
       ),
-
     );
-
   }
-
 }
